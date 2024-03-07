9f7646f2f407c89689ff18e90f8d41465e542f96
dan000317 <120006319+dan000317@users.noreply.github.com>
2024-03-07 17:04:33 +0800

    minor logic correction

--- ip_addr_valid.py
+++ ip_addr_valid.py
@@ -2,11 +2,11 @@ import sys
 
 # Checking validity of each Octet of the IP Address
 # Should not include of the following:
-#   - Loopback: 127.0.0.0 - 127.255.255.255
-#   - Multicast: 224.0.0.0 - 239.255.255.255
-#   - Broadcast: 255.255.255.255
-#   - Link-Local: 169.254.0.0 - 169.254.255.255
-#   - Reserved for future use: 240.0.0.0 - 255.255.255.254
+#   - Loopback: 127.0.0.0 - 127.255.255.255 ok
+#   - Multicast: 224.0.0.0 - 239.255.255.255 ok
+#   - Broadcast: 255.255.255.255 ok
+#   - Link-Local: 169.254.0.0 - 169.254.255.255 ok
+#   - Reserved for future use: 240.0.0.0 - 255.255.255.254 ok
 
 def ip_addr_valid(list):
 
@@ -14,7 +14,7 @@ def ip_addr_valid(list):
         ip = ip.rstrip('\n')
         octet_list = ip.split('.')
     
-        if (len(octet_list) == 4) and (1<= int(octet_list[0]) <= 223) and (int(octet_list[0]) != 127) and (int(octet_list[0])) and ((int(octet_list[0]) != 169) and (int(octet_list[1]) != 254)) and (0 <= int(octet_list[2]) <= 255) and (0 <= int(octet_list[3]) <= 255):
+        if (len(octet_list) == 4) and (1<= int(octet_list[0]) <= 223) and (int(octet_list[0]) != 127) and (int(octet_list[0])) and ((int(octet_list[0]) != 169) or (int(octet_list[1]) != 254)) and (0 <= int(octet_list[1]) <= 255) and (0 <= int(octet_list[2]) <= 255) and (0 <= int(octet_list[3]) <= 255):
             continue
 
         else:
